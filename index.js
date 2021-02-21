c315f40f4f1572b2a5991aed170cf834d2b78149
abduwaly <arsay@foxmail.com>
2021-02-21 12:54:44 +0800

    feat: save crawled pages as html

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
 
+const getAllLinks = async (rootURL) => {
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(rootURL)
 
-
-const crawl = async (rootURL) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(rootURL);
-
-  let urls = await page.evaluate(() => {
+    let urls = await page.evaluate(() => {
       const links = Array.from(document.querySelectorAll('a'))
-      return links.map(link => link.href);
-  });
-  console.log('done', urls);
+      return links.map(link => link.href)
+    })
 
-  await browser.close();
+    await browser.close()
+    return urls
+  } catch (error) {
+    console.error('!err: getAllLinks', error)
+    await browser.close()
+  }
 }
 
-crawl('http://www.uycnr.com/');
+const saveAsHTML = async (urls, folderName) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await urls.forEach(async (url, index) => {    
+    await page.goto(url)
+    let content = await page.content()
+
+    fs.writeFile(
+      `./${folderName}/${index}_${new Date().getTime()}.html`,
+      content.toString(),
+      () => {
+        console.log('✅', index, url);
+      }
+    )
+  })
+}
+
+(async ()=>{
+  const allLinks = await getAllLinks('http://www.uycnr.com/')
+  saveAsHTML(allLinks, 'uycnr')  
+})();
